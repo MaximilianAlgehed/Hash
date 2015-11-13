30553a5b4b467dab439396588d712e5070497441
Maximilian Algehed <m.algehed@gmail.com>
2015-11-13 23:45:52 +0100

    Added pipes

--- Hash.hs
+++ Hash.hs
@@ -2,14 +2,38 @@ import System.Console.Readline
 import System.Directory
 import System.Environment
 import System.Posix.Process
+import System.Posix.IO
 import Data.List.Split
 import Control.Exception
 
 getPaths :: IO [String]
 getPaths = fmap (map (reverse . ((:) '/') . reverse)) $ fmap (splitOn ":") (getEnv "PATH")
 
-executeWith "" = return ()
-executeWith s = executeFile (head (splitOn " " s)) True (tail (splitOn " " s)) Nothing
+executeWith "" _ _ = return ()
+executeWith s in_fd out_fd =
+    do
+        dupTo in_fd stdInput
+        dupTo out_fd stdOutput
+        executeFile (head (splitOn " " s)) True (tail (splitOn " " s)) Nothing
+
+makeProcesses [x] in_fd out_fd =
+    do 
+        pid <- forkProcess (executeWith x in_fd out_fd)
+        return [pid]
+makeProcesses (x:xs) in_fd out_fd = 
+    do
+        (r, w) <- createPipe
+        pid <- forkProcess $ executeWith x in_fd w
+        closeFd w
+        lst <- makeProcesses xs r out_fd
+        closeFd r
+        return (pid:lst)
+
+fixPgms pgms = pgms
+getInput pgms = stdInput
+getOutput pgms = stdOutput
+
+startPrograms pgms = makeProcesses (fixPgms pgms) (getInput pgms) (getOutput pgms)
 
 handleInput :: String -> IO Bool
 handleInput "exit" = return False
@@ -22,8 +46,9 @@ handleInput ('c':'d':xs)
                                 Right () -> return True
 handleInput s =
     do
-        pid <- forkProcess (executeWith s)
-        getProcessStatus True True pid
+        let pgms = splitOn " | " s
+        processes <- startPrograms pgms
+        sequence $ map (\pid -> getProcessStatus True True pid) processes
         return True
 
 readEvalLoop = do
